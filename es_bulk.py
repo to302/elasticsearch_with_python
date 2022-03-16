5f356344037c5c00d82802c8be8ebdff1a47c40e
Kwak SangYoung <to302@nate.com>
2022-03-16 18:48:32 +0900

    bulk 실습, 성공

--- es_bulk.py
+++ es_bulk.py
@@ -2,24 +2,36 @@ from elasticsearch import Elasticsearch
 from elasticsearch import helpers
 import json
 
-es = Elasticsearch( # home version
+es = Elasticsearch( 
     'https://localhost:9200',
-    api_key=('-cN1kX8BAaFJSMrVR1f_', 'Ax84KJycSCG3fXU-VIs_BQ'),
-    ssl_assert_fingerprint=('42376fbf31e025706536cfc90b7f530f01b0d172b0d57622d050d0fb158677bc')
+    api_key=('-cN1kX8BAaFJSMrVR1f_','Ax84KJycSCG3fXU-VIs_BQ'),
+    ca_certs=r'D:\ES\elasticsearch-8.1.0\config\certs\http_ca.crt',
 )
 
 index_name = 'fest-index'
 mapping = None
+
 with open('mapping.json', 'r', encoding='utf-8') as fd:
     mapping = json.load(fd)
 
 if not es.indices.exists(index=index_name):
     es.indices.create(
-        index = index_name,
+        index=index_name,
         mappings = mapping,
     )
+    print('index is created.')
+else:
+    print("index exists.")
 
 
-with open('전국문화축제표준데이터.json','r', encoding='utf-8') as fd:
-    jstr = json.load(fd)['records']
-    print(jstr[1])
+def yield_data():
+    with open('전국문화축제표준데이터.json','r', encoding='utf-8') as fd:
+        jdat = json.load(fd)['records']
+    
+    for i in jdat[0:10]:
+        yield {
+            "_index": index_name,
+            "_source": i,
+        }
+
+helpers.bulk(es, yield_data())
